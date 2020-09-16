c749f932f0be816c36afa86478a18159eb2492d2
saied2035 <70805060+saied2035@users.noreply.github.com>
2020-09-16 17:39:07 +0200

    Update cardList.js

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -4,7 +4,7 @@ const CardList = ({robots}) => {
 	            return (
 	            robots.map( item => {
                    return (
-                      <Card key={itemid} id={itemid} name={item.name} email={item.email} />
+                      <Card key={item.id} id={item.id} name={item.name} email={item.email} />
                   )
 	            }
             )
